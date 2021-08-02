9be361fc5efd4d22cd108d19c2bcf429fef8d2e1
Node-23 <matheus.dev@hotmail.com>
2021-08-01 23:18:15 -0300

    Page style started

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Waystone Inn</h1>
+      <div className="form">
+        <h2>Login</h2>
+        <input type="text"></input>
+        <h2>Password</h2>
+        <input type="password"></input>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/* 
+-TODO-
+ Finish the Page Design (in a way that it continues responsive)
+ Make an animation 
+ Set a background image
+ Set a form icon to login and password form
+ Remake the README
+ Push away h1 from form div
+ Bring the h2 closer to each form 
+ Verify forms (blankspaces and things like it)
+*/
